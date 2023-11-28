7e72ddafada8453a911254be65b749992aafb731
MacPapo <jacopocostantini32@gmail.com>
2023-11-28 14:33:48 +0100

    Added 200Hrz repaint

--- dwm.c
+++ dwm.c
@@ -1301,7 +1301,7 @@ movemouse(const Arg *arg)
 			handler[ev.type](&ev);
 			break;
 		case MotionNotify:
-			if ((ev.xmotion.time - lasttime) <= (1000 / 60))
+			if ((ev.xmotion.time - lasttime) <= (1000 / 200))
 				continue;
 			lasttime = ev.xmotion.time;
 
